f09e51844c9fe5da37f8fa5bbdbb8cc298b70db1
caduzz <67341983+caduzz@users.noreply.github.com>
2024-02-29 11:42:57 -0300

    post create edit

--- src/Services/posts.ts
+++ src/Services/posts.ts
@@ -3,83 +3,82 @@ import { PrismaClient } from "@prisma/client";
 import { IPost, IPostCreate } from "../@types/posts";
 
 interface IResPromise {
-    msg: string
-    sucess: boolean
-    status: number
-    post?: IPost
-    posts?: IPost[]
+	msg: string;
+	sucess: boolean;
+	status: number;
+	post?: IPost;
+	posts?: IPost[];
 }
 
 export default class PostService {
-    private prisma: PrismaClient
+	private prisma: PrismaClient;
 
-    constructor () {
-        this.prisma = new PrismaClient()
-    }
+	constructor() {
+		this.prisma = new PrismaClient();
+	}
 
-    homePosts = async (id: string) => {
-        const posts = await this.prisma.posts.findMany({ 
-            where: {
-                NOT: { userId: id }
-            }, 
-            orderBy: {
-                created_at: "desc"
-            },
-            include: { 
-                user: {
-                    select: {
-                        avatar: true,
-                        username: true,
-                        name: true,
-                        id: true
-                    }
-                }
-            }
-        })
+	homePosts = async (id: string) => {
+		const posts = await this.prisma.posts.findMany({
+			where: {
+				NOT: { userId: id },
+			},
+			orderBy: {
+				created_at: "desc",
+			},
+			include: {
+				user: {
+					select: {
+						avatar: true,
+						username: true,
+						name: true,
+						id: true,
+					},
+				},
+			},
+		});
 
-        return {sucess: true, msg: 'Sucess Get Posts', status: 200, posts}
-    }
+		return { sucess: true, msg: "Sucess Get Posts", status: 200, posts };
+	};
 
-    list = async (id: string) => {
-        const posts = await this.prisma.posts.findMany({
-            where: {
-                userId: id
-            }, 
-            select: {
-                id: true,
-                msg: true,
-                user: {
-                    select: {
-                        id: true,
-                        avatar: true,
-                        name: true,
-                        username: true,
-                        accentColor: true,
-                    }
-                }
-            },
-        })
+	list = async (id: string) => {
+		const posts = await this.prisma.posts.findMany({
+			where: {
+				userId: id,
+			},
+			select: {
+				id: true,
+				msg: true,
+				user: {
+					select: {
+						id: true,
+						avatar: true,
+						name: true,
+						username: true,
+						accentColor: true,
+					},
+				},
+			},
+		});
 
-        return {sucess: true, msg: 'Sucess Get Posts', status: 200, posts}
-    }
+		return { sucess: true, msg: "Sucess Get Posts", status: 200, posts };
+	};
 
-    create = async ({msg, userId}: IPostCreate): Promise<IResPromise> => {
-        const post = await this.prisma.posts.create({
-            data: {
-                msg,
-                userId
-            }
-        })
-        
-        return {sucess: true, msg: 'Sucess Create Post', status: 200}
-    }
-    
-    delete = async (id : string) : Promise<IResPromise> => {
-        const post_delete = await this.prisma.posts.deleteMany({ where: { id } })
-        
-        if(post_delete.count === 1)
-            return {sucess: true, msg: 'Sucess delete Post', status: 200}
-        else
-            return {sucess: false, msg: 'Erro delete Post', status: 400}
-    }   
+	create = async ({ msg, userId }: IPostCreate): Promise<IResPromise> => {
+		const post = await this.prisma.posts.create({
+			data: {
+				msg,
+				userId,
+			},
+		});
+
+		return { sucess: true, msg: "Sucess Create Post", status: 200 };
+	};
+
+	delete = async (id: string): Promise<IResPromise> => {
+		const post_delete = await this.prisma.posts.deleteMany({ where: { id } });
+
+		return post_delete.count === 1
+			? { sucess: true, msg: "Sucess delete Post", status: 200 }
+			: { sucess: false, msg: "Erro delete Post", status: 400 };
+	};
 }
